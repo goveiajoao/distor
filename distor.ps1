fa2bbe04d46749d3e823e2ed14f3818510fce2dc
goveiajoao <joaoantoniogoveia@outlook.com>
2026-08-28 08:47:04 -0300

    all imp.

--- distor.ps1
+++ distor.ps1
@@ -1,48 +1,96 @@
+param (
+	[switch]$i32 = $false,
+	[string]$addr = "127.0.0.1",
+	[string]$port = "9050"
+)
+
 $spi = ">>>"	# Script Print Indicator
-$browserName = "Tor Browser"
 $serviceName = "tor"
-$serviceInfo = Get-Process -Name $serviceName -ErrorAction SilentlyContinue
+$serviceInfo = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+$downloadHub = Invoke-WebRequest -Uri https://www.torproject.org/download/tor/
+$arch = (&{if($i32)
+		{"i686"
+		} else
+		{"x86_64"
+		}})
 
-# Check Browser
-# ~~~ Checking goes here (maybe, first check if winget can make it)
-Write-Host "$spi Installing $browserName" -ForegroundColor Green
-winget install --silent --no-upgrade -e --id TorProject.TorBrowser
 
-# Check Service
+$serviceInfo = $true
 if ($serviceInfo)
 { 
-	Write-Host "$spi '$serviceName' service found."  -ForegroundColor Green
+	Write-Host "$spi '$serviceName' service found"
 
 	if ($serviceInfo.Status -ne "Running")
 	{
-		Write-Host "$spi '$serviceName' service not running, starting it..."  -ForegroundColor Red
+		Write-Host "$spi '$serviceName' service not running, starting it..."
 		Start-Service -Name $serviceName -verbose
 		$serviceInfo.Refresh()
 		Write-Host $serviceInfo.Status
 	} else
 	{
-		Write-Host "$spi '$serviceName' service is running" -ForegroundColor Green
+		Write-Host "$spi '$serviceName' service is running"
 	}
 
 } else
 { 
-	Write-Host "$spi '$serviceName' service not found.\n"  -ForegroundColor Red
-	# ~~~ Tor service installation goes here
+	Write-Host "$spi '$serviceName' service not found, downloading it..."
+	
+	# Take downloadLink
+	$downloadLink = $downloadHub.Links | where {$_ -like "*tor-expert-bundle-windows-$arch*"} | select -first 1
+	if ($downloadLink -match "https://[^`"]+")
+	{
+		$downloadLink = $Matches[0]
+		Write-Host "$spi found tor '$downloadLink'" 
+	} else
+	{
+		Write-Host "$spi didnt find useful tor"
+	}
+
+	
+	# Create tempFolder
+	$tempFolder = "$([System.IO.Path]::GetTempPath())$(New-Guid)"
+	New-Item -Type Directory -Path $tempFolder | Out-Null
+	Write-Host "$spi created temp '$tempFolder'"
+
+
+	# Set and Download torTar
+	$torTar = Join-Path $tempFolder "tor.tar.gz"
+	Invoke-WebRequest -Uri $downloadLink -OutFile $torTar
+	Write-Host "$spi tor downloaded '$torTar'"
+
+	# Unzip torTar
+	tar -xf $torTar -C $tempFolder
+	$tor = Join-Path $tempFolder "tor" "tor.exe"
+	Write-Host "$spi tor unzipped '$tor'"
+	
+	# Install tor
+	if (& $tor -service install)
+	{
+		Write-Host "$spi tor installed"
+	}
+
+
+	# Delete tempFolder
+	Remove-Item -Path $tempFolder -Recurse -Force | Out-Null
+	Write-Host "$spi deleted temp '$tempFolder'"
+
+
 }
 
+
+
 # Change Selecter *.lnk
-# ~~~ Take the *.lnk starting from the user's desktop
-# ~~~ Change its proprieties to "--proxy-server="socks5://127.0.0.1:9050"
+# Take file
+[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+$shortcutDialog = New-Object System.Windows.Forms.OpenFileDialog
+$shortcutDialog.initialDirectory = [Environment]::GetFolderPath("Desktop")
+$shortcutDialog.filter = "Client Shortcut (*.lnk)| *.lnk"
+$shortcutDialog.ShowDialog() | Out-Null
+$shortcutPath = $shortcutDialog.filename
 
-
-
-#
-#	NOTES
-#
-
-# 1) Add flags for:
-# 	- Select each change stage
-# 	- Change tor socks5 ip and port
-# 2) Othe colorizer method with ascii (so the spi dont get colored):
-# 	""" Write-Host "`e[31mThis text is red.`e[0m" """
-#
+# Take and change shortcut
+$wsh = New-Object -ComObject WScript.Shell
+$shortcut = $wsh.CreateShortcut($shortcutPath)
+$shortcut.arguments = "--proxy-server=`"socks5://$addr`:$port`""
+Write-Host "$spi arguments: $shortcut.arguments"
+$shortcut.Save()
