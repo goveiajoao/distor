5095eb7e1a649df7947d97ab1de7c2d99b8daa6f
goveiajoao <joaoantoniogoveia@outlook.com>
2026-08-28 12:24:18 -0300

    minor changes

--- distor.ps1
+++ distor.ps1
@@ -4,7 +4,7 @@ param (
 	[string]$port = "9050"
 )
 
-$spi = ">>>"	# Script Print Indicator
+$prefix = ">>>"
 $serviceName = "tor"
 $serviceInfo = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
 $downloadHub = Invoke-WebRequest -Uri https://www.torproject.org/download/tor/
@@ -15,64 +15,63 @@ $arch = (&{if($i32)
 		}})
 
 
-$serviceInfo = $true
 if ($serviceInfo)
 { 
-	Write-Host "$spi '$serviceName' service found"
+	Write-Host "$prefix '$serviceName' service found"
 
 	if ($serviceInfo.Status -ne "Running")
 	{
-		Write-Host "$spi '$serviceName' service not running, starting it..."
+		Write-Host "$prefix '$serviceName' service not running, starting it..."
 		Start-Service -Name $serviceName -verbose
 		$serviceInfo.Refresh()
 		Write-Host $serviceInfo.Status
 	} else
 	{
-		Write-Host "$spi '$serviceName' service is running"
+		Write-Host "$prefix '$serviceName' service is running"
 	}
 
 } else
 { 
-	Write-Host "$spi '$serviceName' service not found, downloading it..."
+	Write-Host "$prefix '$serviceName' service not found, downloading it..."
 	
 	# Take downloadLink
 	$downloadLink = $downloadHub.Links | where {$_ -like "*tor-expert-bundle-windows-$arch*"} | select -first 1
 	if ($downloadLink -match "https://[^`"]+")
 	{
 		$downloadLink = $Matches[0]
-		Write-Host "$spi found tor '$downloadLink'" 
+		Write-Host "$prefix found tor '$downloadLink'" 
 	} else
 	{
-		Write-Host "$spi didnt find useful tor"
+		Write-Host "$prefix didnt find useful tor"
 	}
 
 	
 	# Create tempFolder
 	$tempFolder = "$([System.IO.Path]::GetTempPath())$(New-Guid)"
 	New-Item -Type Directory -Path $tempFolder | Out-Null
-	Write-Host "$spi created temp '$tempFolder'"
+	Write-Host "$prefix created temp '$tempFolder'"
 
 
 	# Set and Download torTar
 	$torTar = Join-Path $tempFolder "tor.tar.gz"
 	Invoke-WebRequest -Uri $downloadLink -OutFile $torTar
-	Write-Host "$spi tor downloaded '$torTar'"
+	Write-Host "$prefix tor downloaded '$torTar'"
 
 	# Unzip torTar
 	tar -xf $torTar -C $tempFolder
 	$tor = Join-Path $tempFolder "tor" "tor.exe"
-	Write-Host "$spi tor unzipped '$tor'"
+	Write-Host "$prefix tor unzipped '$tor'"
 	
 	# Install tor
 	if (& $tor -service install)
 	{
-		Write-Host "$spi tor installed"
+		Write-Host "$prefix tor installed"
 	}
 
 
 	# Delete tempFolder
 	Remove-Item -Path $tempFolder -Recurse -Force | Out-Null
-	Write-Host "$spi deleted temp '$tempFolder'"
+	Write-Host "$prefix deleted temp '$tempFolder'"
 
 
 }
@@ -92,5 +91,5 @@ $shortcutPath = $shortcutDialog.filename
 $wsh = New-Object -ComObject WScript.Shell
 $shortcut = $wsh.CreateShortcut($shortcutPath)
 $shortcut.arguments = "--proxy-server=`"socks5://$addr`:$port`""
-Write-Host "$spi arguments: $shortcut.arguments"
+Write-Host "$prefix arguments: $shortcut.arguments"
 $shortcut.Save()
